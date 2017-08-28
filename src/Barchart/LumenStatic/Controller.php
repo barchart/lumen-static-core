552ddd200bc08acbbaf67e27870002c02a1dcb4b
Tom Sisk <tom.sisk@barchart.com>
2017-08-28 20:44:58 +0000

    Support index files in directories Also changes default 'home' to 'index'

--- src/Barchart/LumenStatic/Controller.php
+++ src/Barchart/LumenStatic/Controller.php
@@ -2,28 +2,29 @@
 
 namespace Barchart\LumenStatic;
 
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    public function get($route)
+    public function get(Request $request, $route = '')
     {
-        $view = $this->getViewName($route);
-
-        if (! view()->exists($view)) {
-            abort(404);
-        }
-
-        return view($view);
+        return view($this->getViewName($request->path()));
     }
 
     protected function getViewName($route)
     {
-        // TODO: set template based on website being delivered.
-        if (empty($route)) {
-            return 'pages.home';
-        }
+        $view = ($route === '/') ? 'pages.index' : 'pages.' . str_replace('/', '.', $route);
 
-        return 'pages.' . str_replace('/', '.', $route);
+        // If view does not exist, try the index view if it has not been attempted already.
+        if (! view()->exists($view)) {
+            if (! ends_with($view, 'index')) {
+                return $this->getViewName($route.'/index');
+            } else {
+                abort(404);
+            }
+        }
+        
+        return $view;
     }
 }
